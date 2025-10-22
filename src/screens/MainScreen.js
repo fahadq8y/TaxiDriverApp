621c633760df8c062e9c778dd516ab1ef2e01f3f
fahadq8y <214653162+fahadq8y@users.noreply.github.com>
2025-10-22 11:59:48 -0400

    Fix: Use driverId instead of userId in WebView URL
    This fixes the 'Driver Inactive' issue in the tracking page.
    
    Problem:
    - LocationService saves location to: drivers/{driverId}
    - WebView was reading from: drivers/{userId}
    - Result: Page couldn't find driver data
    
    Solution:
    - Changed WebView URL parameter from userId to driverId
    - Now both save and read use the same document ID
    
    File changed:
    - src/screens/MainScreen.js: Line 246

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -243,7 +243,7 @@ const MainScreen = ({ navigation, route }) => {
     );
   }
 
-  const webViewUrl = `https://taxi-management-system-d8210.web.app/driver-view.html?driverId=${userId}`;
+  const webViewUrl = `https://taxi-management-system-d8210.web.app/driver-view.html?driverId=${driverId}`;
 
   return (
     <>
