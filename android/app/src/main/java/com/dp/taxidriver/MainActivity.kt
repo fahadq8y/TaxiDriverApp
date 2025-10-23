cbb17ea38bad4d714c35bcb42f843ec8615605e2
fahadq8y <214653162+fahadq8y@users.noreply.github.com>
2025-10-23 19:25:39 -0400

    Fix: Add Notification Channel creation for Android 8+ Foreground Service

--- android/app/src/main/java/com/dp/taxidriver/MainActivity.kt
+++ android/app/src/main/java/com/dp/taxidriver/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.dp.taxidriver
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,5 +23,37 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  /**
+   * Create notification channel for Android 8.0+ (API 26+)
+   * This is REQUIRED for Foreground Service to work properly
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    createNotificationChannel()
+  }
+
+  private fun createNotificationChannel() {
+    // Only create channel for Android 8.0+ (API 26+)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      val channelId = "location_tracking_channel"
+      val channelName = "Location Tracking"
+      val channelDescription = "Notification channel for location tracking service"
+      val importance = NotificationManager.IMPORTANCE_LOW
+      
+      val channel = NotificationChannel(channelId, channelName, importance).apply {
+        description = channelDescription
+        // Don't make sound or vibration for this channel
+        setSound(null, null)
+        enableVibration(false)
+      }
+      
+      // Register the channel with the system
+      val notificationManager = getSystemService(NotificationManager::class.java)
+      notificationManager?.createNotificationChannel(channel)
+      
+      android.util.Log.d("MainActivity", "Notification channel created: $channelId")
+    }
+  }
 }
 
